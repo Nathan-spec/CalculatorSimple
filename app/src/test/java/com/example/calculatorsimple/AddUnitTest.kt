4ebcb2a8f8d2c89cb7174bdcfe1877621ba03ddf
MamboBryan <mambobryan@gmail.com>
2022-06-15 15:31:47 +0300

    adds five more tests

--- app/src/test/java/com/example/calculatorsimple/AddUnitTest.kt
+++ app/src/test/java/com/example/calculatorsimple/AddUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.calculatorsimple
 import com.example.calculatorsimple.data.add
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import kotlin.random.Random
 
 /**
  * Calculator Simple
@@ -86,27 +87,100 @@ class AddUnitTest {
 
     @Test
     fun add_int_varags_is_correct() {
-        val number = add(1,2,3,4,5)
+        val number = add(1, 2, 3, 4, 5)
         assertEquals(15, number)
     }
 
     @Test
     fun add_float_varags_is_correct() {
-        val number = add(1f,2f,3f,4f,5f)
+        val number = add(1f, 2f, 3f, 4f, 5f)
         assertEquals(15f, number)
     }
 
 
     @Test
     fun add_double_varags_is_correct() {
-        val number = add(1.0,2.0,3.0,4.0,5.0)
+        val number = add(1.0, 2.0, 3.0, 4.0, 5.0)
         assertEquals(15.0, number)
     }
 
     @Test
     fun add_varags_is_correct() {
-        val number = add(1,2f,3L,4.0,5)
+        val number = add(1, 2f, 3L, 4.0, 5)
         assertEquals(15.0, number)
+    }
+
+    @Test
+    fun add_varags_is_correct_int() {
+
+        val list = mutableListOf<Int>()
+
+        for (number in 0 until 10) {
+            list.add(Random.nextInt())
+        }
+
+        val number = add(list)
+        assertEquals(list.sum(), number)
+    }
+
+    @Test
+    fun add_varags_is_correct_double() {
+
+        val list = mutableListOf<Double>()
+
+        for (number in 0 until 10) {
+            list.add(Random.nextDouble())
+        }
+
+        val number = add(list)
+        assertEquals(list.sum(), number)
+    }
+
+    @Test
+    fun add_varags_is_correct_float() {
+
+        val list = mutableListOf<Float>()
+
+        for (number in 0 until 10) {
+            list.add(Random.nextFloat())
+        }
+
+        val number = add(list)
+        assertEquals(list.sum(), number)
+
+    }
+
+    @Test
+    fun add_varags_is_correct_long() {
+
+        val list = mutableListOf<Long>()
+
+        for (number in 0 until 10) {
+            list.add(Random.nextLong())
+        }
+
+        val number = add(list)
+        assertEquals(list.sum(), number)
+
+    }
+
+    @Test
+    fun add_varags_is_correct_numbers() {
+
+        val ints = mutableListOf<Int>()
+        val doubles = mutableListOf<Double>()
+
+        for (number in 0 until 5) {
+            ints.add(Random.nextInt())
+        }
+
+        for (number in 0 until 5) {
+            doubles.add(Random.nextDouble())
+        }
+
+        val number = add(ints, doubles)
+        assertEquals(ints.sum() + doubles.sum(), number)
+
     }
 
 }
